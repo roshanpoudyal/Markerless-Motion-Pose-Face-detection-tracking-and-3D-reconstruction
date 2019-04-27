630f7d70d0589e4b7a6e2e56c5181b2d08a38f01
roshanpoudyal <roshanpoudyal.di@gmail.com>
2019-04-27 14:01:45 +0200

    opencv application to calibrate camera started

---
+++ opencv_projects/src/camera_calibration.cpp
@@ -0,0 +1,37 @@
+/*
+* TODO : camera calibration
+* ref : https://docs.opencv.org/4.1.0/dc/dbb/tutorial_py_calibration.html
+* ref : https://www.cds.caltech.edu/~murray/wiki/images/6/68/Assignment_1_MECS132A.pdf
+*/
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+
+int main()
+{
+    VideoCapture live_camera(0); // let us take live feed from camera
+
+    if(!live_camera.isOpened()) // check if we succeded in opening camera
+    {
+        std::cout << "error opening live camera" << std::endl;
+        return -1;
+    }
+
+    namedWindow("livecamera",1); // initialize a named window to display live camera feed
+
+    // display new live camera frame on every iteration of the never ending loop such that we see a video
+    while(1)
+    {
+        Mat live_camera_frame; // create a Mat object to store single frame from live camera feed
+        live_camera >> live_camera_frame; // get new frame from camera for this loop iteration
+
+        imshow("livecamera", live_camera_frame); // show this live camera frame in our previously initialized named window
+
+        // create a condition to break this never ending loop : if ESC (ASCII code is 27) key is pressed
+        if(waitKey(30) == 27 ) break;
+    }
+
+    return 0;
+}
--- opencv_projects/src/video_read_offline_online_opencv.cpp
+++ opencv_projects/src/video_read_offline_online_opencv.cpp
@@ -15,23 +15,24 @@ int main()
     VideoCapture live_camera(0); // use this format to read live (online) camera device - usually the device id start from 0, if you have more camera it can +1 on the id
     // VideoCapture offline_video("some_video_filename.some_video_format"); // use this format if you want to read from a video file
 
-    if(!live_camera.isOpened()) // check if we succeded in opening camera of video
+    if(!live_camera.isOpened()) // check if we succeded in opening camera for video or video file
     {
         std::cout << "error opening live camera stream or file" << std::endl;
         return -1;
     }
 
-    namedWindow("live_camera",1); // create a named window to display live camera frame or video frames
+    namedWindow("live_camera",1); // initialize a named window to display live camera feed
 
-    // run a loop continiously until last frame or until a key is pressed
-    while(1){
+    // display new live camera frame on every iteration of the never ending loop such that we see a video
+    while(1)
+    {
         Mat live_camera_frame; // Create new mat object to store an image
-        live_camera >> live_camera_frame; // get a new frame from camera
-        imshow("live_camera", live_camera_frame); // show each new live camera frame in the named window we created before
+        live_camera >> live_camera_frame; // get new frame from camera for this loop iteration
+        imshow("live_camera", live_camera_frame); // show this live camera frame in our previously initialized named window 
 
-        // if an escape key is pressed or video stream online (live) or offline is finished we break from the loop
-        if(waitKey(30) >=0 || live_camera_frame.empty()) break;
-        // if(waitKey(30) >=0 || offline_video_stream.empty()) break;
+        // if an ESC (ASCII code is 27) key is pressed or video stream online (live) or offline is finished we break from the loop
+        if(waitKey(30) == 27 || live_camera_frame.empty()) break;
+        // if(waitKey(30) == 27 || offline_video_stream.empty()) break;
     }
     
     return 0;

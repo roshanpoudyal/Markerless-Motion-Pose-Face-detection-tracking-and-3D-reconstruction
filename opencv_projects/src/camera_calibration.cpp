ea9201d34c1c143e4fb38a650860617beda131f5
roshanpoudyal <roshanpoudyal.di@gmail.com>
2019-05-01 17:08:03 +0200

    finding and refining corners on chessboard pattern done, also drawn the found chessboard corners and shown in a separate window

--- opencv_projects/src/camera_calibration.cpp
+++ opencv_projects/src/camera_calibration.cpp
@@ -3,13 +3,16 @@
 * ref : https://docs.opencv.org/4.1.0/dc/dbb/tutorial_py_calibration.html
 * ref : https://www.cds.caltech.edu/~murray/wiki/images/6/68/Assignment_1_MECS132A.pdf
 * ref : https://stackoverflow.com/a/10346021
+* ref : https://docs.opencv.org/4.1.0/d9/d0c/group__calib3d.html#ga93efa9b0aa890de240ca32b11253dd4a : sample usage of detecting and drawing chessboard corners section in this link
+* ref : https://docs.opencv.org/4.1.0/dd/d1a/group__imgproc__feature.html#ga354e0d7c86d0d9da75de9b9701a9a87e : for cornerSubPix() function definition
+* ref : https://docs.opencv.org/3.4/da/d54/group__imgproc__transform.html#ga47a974309e9102f5f08231edc7e7529d : resize() api for image in opencv
 */
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 
-using namespace cv;
+using namespace cv; // namespace used for opencv
 
 int main()
 {
@@ -21,7 +24,10 @@ int main()
         return -1;
     }
 
-    namedWindow("livecamera",1); // initialize a named window to display live camera feed
+    namedWindow("livecamera", WINDOW_NORMAL); // initialize a named window to display live camera feed
+    resizeWindow("livecamera", 426, 240); // resize the window to smaller size
+    namedWindow("drawcorners", WINDOW_NORMAL); // initialze a named window to display chessboard with corners drawn on it
+    resizeWindow("drawcorners", 426, 240); // resize the window to smaller size
 
     // create cv::Point3f vector to store 3D object points from all images
     std::vector<std::vector<Point3f>> objectPoints;
@@ -29,26 +35,59 @@ int main()
     // create cv::Point2f vector to store 2D image points from all images
     std::vector<std::vector<Point2f>> imagePoints;
 
+    // create Size object instance that will store the pattern size of our input chessboard image
+    // pattern size is the number of corners in a row by number of corners in a column
+    Size chessboard_pattern_size(7,7); 
+
     // display new live camera frame on every iteration of the never ending loop such that we see a video
     while(1)
     {
-        Mat live_camera_frame; // create a Mat object to store single frame from live camera feed
+        Mat original_live_camera_frame; // create a Mat object to store single frame from live camera feed
+        Mat live_camera_frame; // resized live camera frame
         Mat gray_live_camera_frame; // Mat object to store the live camera frame converted to grayscale
-        live_camera >> live_camera_frame; // get new frame from camera for this loop iteration
+
+        live_camera >> original_live_camera_frame; // get new frame from our initialized videocapture live_camera for this loop iteration
         
-        cvtColor(live_camera_frame, gray_live_camera_frame, COLOR_BGR2GRAY);
+        // resize the live feed to 320 width and 240 height image , we do not need other optional parameters of this resize api
+        resize(original_live_camera_frame, live_camera_frame, Size(426,240)); 
+
+        cvtColor(live_camera_frame, gray_live_camera_frame, COLOR_BGR2GRAY); // convert the color of live camera frames to grayscale
+
         imshow("livecamera", gray_live_camera_frame); // show this live camera frame in our previously initialized named window
 
+        // create vector of opencv type Point2f to store detected corners it also will store refined corners from cornerSubPix() api
+        std::vector<Point2f> chessboard_corners;
+
+        // now let us find chess board corners and return the status if found as well
+        bool chessboard_pattern_found = findChessboardCorners(gray_live_camera_frame, 
+                                                    chessboard_pattern_size,
+                                                    chessboard_corners,
+                                                    CALIB_CB_ADAPTIVE_THRESH
+                                                    + CALIB_CB_NORMALIZE_IMAGE
+                                                    + CALIB_CB_FAST_CHECK);
+
         // create a condition to break this never ending loop : if ESC (ASCII code is 27) key is pressed
-        if(waitKey(30) == 27 )
+        if(waitKey(30) == 27 ) 
         {
-            destroyWindow("livecamera");    
-            break;
+            destroyAllWindows();   
+            break; // break the while loop
         }
-        else // if live stream does not break go on with remaining code
+        else if(chessboard_pattern_found) // if chessboard pattern is found do these computations in this scope 
         {
-            // some code
-            null;
+            std::cout << "chessboard pattern found" << std::endl;
+
+            // the detected corners with findChessboardCorners() function is approximate
+            // cornerSubPix() function iterates to find the sub-pixel accurate location of corners
+            // and refines corner locations for more accuracy
+            cornerSubPix(gray_live_camera_frame, chessboard_corners, Size(11,11), Size(-1,-1),
+                    TermCriteria(TermCriteria::EPS + TermCriteria::MAX_ITER, 30, 0.1));
+            
+            // now draw chess board corners on every frame a pattern is found but on not grayscale but colorful video frame
+            // so that the drawing is seen colorful
+            drawChessboardCorners(live_camera_frame, chessboard_pattern_size, chessboard_corners, chessboard_pattern_found);
+
+            imshow("drawcorners", live_camera_frame);
+            
         }        
     }
     return 0;
